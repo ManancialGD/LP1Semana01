9af3a6d7c0fcfffe3557e6c264a33889a607bb08
“[Breno <manancialgd@gmail.com>
2024-02-21 21:28:10 +0000

    Created different types of int variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -4,6 +4,13 @@ namespace VariousTypes
 {
     class Program
     {
+        int a = 1;
+        int b = 0x10;
+        int c = 0x10U;
+        int d = 0b10110110;
+        int e = 0b1011_0110;
+        int f = 0xFF_2B_4A;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
