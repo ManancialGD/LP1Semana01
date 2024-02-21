51cf134df6bb8247272f370efa2441b495869b14
“[Breno <manancialgd@gmail.com>
2024-02-21 12:38:55 +0000

    Added some comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+/// <summary>
+/// This is my program for the first week of LP1.
+/// </summary>
     class Program
     {
+        /// <summary>
+        /// This void main will write in the console the below.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
