fc14b890417780a2ea13a0c8a31290119397a647
“[Breno <manancialgd@gmail.com>
2024-02-23 17:08:14 +0000

    added 2 more variables and wrote them with different types of formatation

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -13,6 +13,8 @@ namespace ManyStrings
 
             int x = 5;
             float y = 2.2f;
+            double xx = 1.23456;
+            int ii = 19;
 
             string message = "Hello, World!";
             string s = "Um tab\t e um copyright \u03A9";
@@ -21,12 +23,20 @@ namespace ManyStrings
             string s4 = $"{x} mais {y}  igual a {x + y}";
 
 
-            // Write on console
+            // Write strings on console
             Console.WriteLine(message);
             Console.WriteLine(s);
             Console.WriteLine(s2);
             Console.WriteLine(s3);
             Console.WriteLine(s4);
+
+            // Write xx
+            Console.WriteLine($"Número real com duas casas decimais: {xx:F2}");
+            Console.WriteLine($"Percentagem com uma casa decimal: {xx:P1}");
+
+            // Write ii
+            Console.WriteLine($"Hexadecimal: 0x{ii:X}");
+            Console.WriteLine($"Moeda: {ii:C}");
         }
     }
 }
