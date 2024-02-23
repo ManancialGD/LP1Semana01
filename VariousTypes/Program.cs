df9349255c290090e335a42123a4a99a01938281
“[Breno <manancialgd@gmail.com>
2024-02-23 16:36:45 +0000

    Remove one int variable, added come char variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -7,18 +7,28 @@ namespace VariousTypes
         
         static void Main(string[] args)
         {
+            // Int variables
             int intA = 1;
             int intB = 0x10;
             uint uintC = 0x10U;
             int intD = 0b10110110;
-            int intE = 0b1011_0110;
-            int intF = 0xFF_2B_4A;
+            int intE = 0xFF_2B_4A;
+
+            // char variables
+
+             char copyrightSymbol = '\u00A9';
+             char newLine = '\u000A';
+
+            Console.WriteLine("These are int types:");
             Console.WriteLine(intA);
             Console.WriteLine(intB);
             Console.WriteLine(uintC);
             Console.WriteLine(intD);
             Console.WriteLine(intE);
-            Console.WriteLine(intF);
+
+            Console.WriteLine("These are chat Types");
+            Console.WriteLine(copyrightSymbol);
+            Console.WriteLine(newLine);
         }
     }
 }
